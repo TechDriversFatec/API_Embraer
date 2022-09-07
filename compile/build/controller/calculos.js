c345be9549309f504cc8eddc4d8e4c21aeb01180
Icaro Dell Osso <icaro.dellosso@hotmail.com>
2022-09-07 14:14:53 -0300

    .

--- compile/build/controller/calculos.js
+++ compile/build/controller/calculos.js
@@ -1,6 +1,8 @@
 export default class Calculos {
     distanciaPouso(aeronave, pesoAtual, alturaAtual, temperaturaAtual, ventoCabeca, ventoCauda) {
         let distanciaReferencia = 1026;
+        let chao = 0;
+        let padraoIsa = 0;
         if (pesoAtual > aeronave.getPeso) {
             while (pesoAtual > aeronave.getPeso) {
                 distanciaReferencia += 16;
@@ -13,18 +15,35 @@ export default class Calculos {
                 pesoAtual += 1000;
             }
         }
-        distanciaReferencia += 26;
-        if (temperaturaAtual > 5) {
-            distanciaReferencia += 18;
+        if (alturaAtual > chao) {
+            while (alturaAtual > chao) {
+                distanciaReferencia += 26;
+                alturaAtual -= 1000;
+            }
         }
-        else {
-            distanciaReferencia -= 10;
+        if (temperaturaAtual > padraoIsa) {
+            while (temperaturaAtual > padraoIsa) {
+                distanciaReferencia += 18;
+                temperaturaAtual -= 5;
+            }
+        }
+        if (temperaturaAtual < padraoIsa) {
+            while (temperaturaAtual < padraoIsa) {
+                distanciaReferencia -= 10;
+                temperaturaAtual += 5;
+            }
         }
         if (ventoCabeca > 5) {
-            distanciaReferencia += 22;
+            while (ventoCabeca > 5) {
+                distanciaReferencia -= 22;
+                ventoCabeca -= 5;
+            }
         }
         if (ventoCauda > 5) {
-            distanciaReferencia += 101;
+            while (ventoCauda > 5) {
+                distanciaReferencia += 101;
+                ventoCauda -= 5;
+            }
         }
     }
 }
--- src/controller/calculos.ts
+++ src/controller/calculos.ts
@@ -5,7 +5,10 @@ export default class Calculos {
     distanciaPouso(aeronave: Aeronave, pesoAtual: number, alturaAtual:number, temperaturaAtual: number, ventoCabeca: number, ventoCauda: number, ){
 
         let distanciaReferencia: number = 1026
+        let chao: number = 0
+        let padraoIsa: number = 0
 
+        //calculo do peso
         if (pesoAtual > aeronave.getPeso){
             while (pesoAtual > aeronave.getPeso){
                 distanciaReferencia += 16
@@ -19,20 +22,40 @@ export default class Calculos {
             }
         }
 
-        distanciaReferencia += 26;
+        //calculo da altura
+        if (alturaAtual > chao){
+            while (alturaAtual > chao){
+                distanciaReferencia += 26
+                alturaAtual -= 1000
+            }
+        } 
 
-        if (temperaturaAtual > 5){
-            distanciaReferencia += 18;
-        } else {
-            distanciaReferencia -= 10;
+        //calculo da temperatura
+        if (temperaturaAtual > padraoIsa){
+            while (temperaturaAtual > padraoIsa){
+                distanciaReferencia += 18
+                temperaturaAtual -= 5
+            }
+        }
+        if (temperaturaAtual < padraoIsa){
+            while (temperaturaAtual < padraoIsa){
+                distanciaReferencia -= 10
+                temperaturaAtual += 5
+            }
         }
 
+        //calculo do vento
         if (ventoCabeca > 5){
-            distanciaReferencia += 22
+            while (ventoCabeca > 5){
+                distanciaReferencia -= 22
+                ventoCabeca -= 5
+            }
         }
-
         if (ventoCauda > 5){
-            distanciaReferencia += 101
+            while (ventoCauda > 5){
+                distanciaReferencia += 101
+                ventoCauda -= 5
+            }
         }
 
     }
