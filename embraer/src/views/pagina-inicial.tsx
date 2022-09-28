01b18bb4da9193f32495910a874d0c239454573f
Yasmin Vieira <yasmin.vieira@pro4tech.com.br>
2022-09-28 11:27:45 -0300

    Co-authored-by: LARISSA APARECIDA DINIZ SILVA <larissa.silva179@fatec.sp.gov.br>

---
+++ embraer/src/views/pagina-inicial.tsx
@@ -0,0 +1,64 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Component } from "react"
+import Table from 'react-bootstrap/Table';
+
+class PaginaInicial extends Component{
+    render(){
+        return (
+            <div className="App">
+              <div>
+                <h2>
+                    Gerenciar informações
+                </h2>
+              </div>
+              <div>
+              <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>Modelo</th>
+                  <th>Fabricante</th>
+                  <th>Motor</th>
+                  <th>Peso Referencial</th>
+                  <th>Quantidade de Reversores</th>
+                  <th>Ações</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>E-195</td>
+                  <td>-</td>
+                  <td>-</td>
+                  <td>-</td>
+                  <td>-</td>
+                  <td>-</td>
+                </tr>
+                <tr>
+                  <td>E-190</td>
+                  <td>-</td>
+                  <td>-</td>
+                  <td>-</td>
+                  <td>-</td>
+                  <td>-</td>
+                </tr>
+              </tbody>
+            </Table>
+              </div>
+              <div>
+                <button
+                  className="rounded"
+                  type="submit"
+                  id="btn_calcular"
+                  name="submitButton"
+                >
+                  <b>Registrar Aeronave</b>
+                </button>
+                <input hidden placeholder="register" type="text" id="registrarAeronave"/>
+              </div>
+              <script src="../compile/build/app.js"></script>
+            </div>
+          );
+    }
+}
+  
+export default PaginaInicial;
