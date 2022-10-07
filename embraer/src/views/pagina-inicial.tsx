f9085e7c6f94238ddbdf7852840e61a8656f4aae
Icaro Dell Osso <icaro.dellosso@hotmail.com>
2022-10-07 10:30:10 -0300

    Alteração da tabela

--- embraer/src/views/pagina-inicial.tsx
+++ embraer/src/views/pagina-inicial.tsx
@@ -2,11 +2,24 @@
 import "../css/pagina-inicial.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Table from "../controller/tabela";
+import { Container } from "react-bootstrap";
 
 function PaginaInicial() {
   return (
       <div className="App">
+        <div>
+        <h2>
+          <i>
+            <img src="loguinho.png" id="logoAviaozinho" alt="some text" />
+          </i>
+        </h2>
+      </div>
+
+        <Container>
         <Table />
+        </Container>
+        
+        <footer>Todos os direitos reservados.</footer>
       </div>
   );
 }
