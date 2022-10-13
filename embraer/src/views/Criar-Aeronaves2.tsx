604d05c4fa0584d0ca3ef0a5b80a33ef41175602
Guilherme Konflanz Mezzomo <guizzomo@gmail.com>
2022-10-13 09:15:08 -0300

    quebra de linha

--- embraer/src/views/Criar-Aeronaves2.tsx
+++ embraer/src/views/Criar-Aeronaves2.tsx
@@ -234,6 +234,7 @@ function CriarAeronaves() {
                         <div className="row">
                             <div className="form-group col-lg-4-md col-md-6 col-sm-12">
                                 <label id="label-vap">Vap (speed):</label>
+                                <br></br>
                                 <label>Insert the speed pattern for landing:</label>
                                 <input id="velocidade_referencia" className="form-control" name="velocidade_referencia"
                                     placeholder="Insert the speed pattern for landing" onChange={handleChangeValues} />
